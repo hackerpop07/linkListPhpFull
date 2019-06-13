6f05fc7cedc3ffdad25cafb9415ca6196574f246
hackerpop07 <hackerpop07@gmail.com>
2019-06-13 20:48:07 +0700

    php

---
+++ ListNode.php
@@ -0,0 +1,23 @@
+<?php
+
+class ListNode
+{
+    /* Data to hold */
+    public $data;
+
+    /* Link to next node */
+    public $next;
+
+
+    /* Node constructor */
+    function __construct($data)
+    {
+        $this->data = $data;
+        $this->next = NULL;
+    }
+
+    function readNode()
+    {
+        return $this->data;
+    }
+}
